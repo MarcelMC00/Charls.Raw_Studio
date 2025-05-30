87c95b09a1afe2b5fed6365a04d6483f5cc35d79
MarsiQueen <marmanica@alu.edu.gva.es>
2025-05-30 19:25:29 +0200

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('400ms ease-out', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('400ms ease-in', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class App {
   protected title = 'Charls_Raw';
+
+  show = true;
+  toggle() {
+    console.log('Toggling...');
+    this.show = !this.show;
+  }
 }
